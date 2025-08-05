194ce745706a856a8b3b1caacfc1e8965c15d7df
monblan <monblan57@gmail.com>
2025-08-05 16:46:23 +0300

    nitial commit: dd website with slider and styling

---
+++ scripts.js
@@ -0,0 +1,40 @@
+jQuery(document).ready(function($){
+    var sliderLeft = $('.slider-left');
+    var sliderRight = $('.slider-right');
+    var heroSection = $('.hero');
+
+    sliderLeft.on('init', function(event, slick){
+        checkSlidersInitialized();
+    });
+
+    sliderRight.on('init', function(event, slick){
+        checkSlidersInitialized();
+    });
+
+    sliderLeft.slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-right',
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 6000,
+    });
+
+    sliderRight.slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.slider-left',
+        arrows: false,
+        focusOnSelect: true,
+        autoplay: true,
+        autoplaySpeed: 6000,
+    });
+
+    function checkSlidersInitialized() {
+        if (sliderLeft.hasClass('slick-initialized') && sliderRight.hasClass('slick-initialized')) {
+            heroSection.css('visibility', 'visible');
+        }
+    }
+});
